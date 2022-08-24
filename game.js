d5374d1b9e5a418a8d7f5a56cd5d452649e34984
Siddhant <78287897+sid12701@users.noreply.github.com>
2022-08-24 12:46:17 +0530

    Add files via upload
    Added all the base files for the game including the sounds

---
+++ game.js
@@ -0,0 +1,65 @@
+var buttoncolours = ["red","blue","green","yellow"];
+var gamepattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(document).keydown(function(){
+  if(!started){
+    $("#level-title").text("Level "+level);
+    nextSequence();
+    started=true;
+  }
+
+});
+
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+
+  playsound(userChosenColour);
+
+  animatepress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+})
+
+
+function nextSequence(){
+  level++;
+  $("#level-title").text("Level "+level);
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosencolour = buttoncolours[randomNumber];
+  gamepattern.push(randomChosencolour);
+  $("#" + randomChosencolour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playsound(randomChosencolour);
+
+}
+
+
+function playsound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+
+function animatepress(currentcolour){
+  $("."+currentcolour).addClass("pressed");
+  setTimeout(function(){
+    $("."+currentcolour).removeClass("pressed");
+  }
+  ,100);
+}
+
+
+function checkAnswer(currentlevel){
+  if (gamepattern[currentlevel]===userClickedPattern[currentlevel]){
+    if (gamepattern.length === userClickedPattern.length){
+      setTimeout(function(){
+        nextSequence();
+      },1000);
+      userClickedPattern=[];
+    }
+  }
+}
